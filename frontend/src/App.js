fbbfd560f6bf61be0018b2723954e34fb74136d7
sahil <sahilarneja2003@gmail.com>
2024-08-31 00:06:52 +0530

    gogle analytics & SEO added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from'react';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import Home from './pages/Home.jsx';
+
+// Initialize Google Analytics with your tracking ID
+ReactGA.initialize('G-41BQ0M7WMB');
+
+function PageTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send('pageview');
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
-    
     <BrowserRouter>
-    <Routes>
-      <Route path = "/" element={<Home />}/>
-    </Routes>
-    
+      <PageTracker /> {/* This component handles page tracking */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </BrowserRouter>
-    
-    
   );
 }
 
